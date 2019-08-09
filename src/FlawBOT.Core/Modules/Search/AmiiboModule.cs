f5ef8cdf16985b23bd1d9dd66ec2d5c90cfc409b
CriticalFlaw <nik_101@live.co.uk>
2019-08-09 12:09:28 -0400

    Added Amiibo Command
    Added a command for retrieving specified amiibo information

---
+++ src/FlawBOT.Core/Modules/Search/AmiiboModule.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using FlawBOT.Framework.Models;
+using FlawBOT.Framework.Services;
+
+namespace FlawBOT.Modules
+{
+    [Cooldown(3, 5, CooldownBucketType.Channel)]
+    public class AmiiboModule : BaseCommandModule
+    {
+        #region COMMAND_AMIIBO
+
+        [Command("amiibo")]
+        [Aliases("amib")]
+        [Description("Retrieve Amiibo figurine information")]
+        public async Task GetAmiibo(CommandContext ctx,
+            [Description("Name of the Amiibo figurine")] [RemainingText] string query)
+        {
+            if (!BotServices.CheckUserInput(query)) return;
+            var results = await AmiiboService.GetAmiiboFigurineAsync(query);
+            if (results == null)
+                await BotServices.SendEmbedAsync(ctx, "No results found!", EmbedType.Missing);
+            else
+            {
+                foreach (var amiibo in results.Amiibo)
+                {
+                    var output = new DiscordEmbedBuilder()
+                        .WithTitle(amiibo.Name)
+                        .AddField("Amiibo Series", amiibo.AmiiboSeries, true)
+                        .AddField("Game Series", amiibo.GameSeries, true)
+                        .AddField(":flag_us: Release:", amiibo.ReleaseDate.American, true)
+                        .AddField(":flag_jp: Release:", amiibo.ReleaseDate.Japanese, true)
+                        .AddField(":flag_eu: Release:", amiibo.ReleaseDate.European, true)
+                        .AddField(":flag_au: Release:", amiibo.ReleaseDate.Australian, true)
+                        .WithImageUrl(amiibo.Image)
+                        .WithFooter("Type next in the next 10 seconds the next amiibo")
+                        .WithColor(new DiscordColor("#E70009"));
+                    var message = await ctx.RespondAsync(embed: output.Build());
+
+                    var interactivity = await ctx.Client.GetInteractivity().WaitForMessageAsync(m => m.Channel.Id == ctx.Channel.Id && m.Content.ToLowerInvariant() == "next", TimeSpan.FromSeconds(10));
+                    if (interactivity.Result == null) break;
+                    await BotServices.RemoveMessage(interactivity.Result);
+                    await BotServices.RemoveMessage(message);
+                }
+            }
+        }
+
+        #endregion COMMAND_AMIIBO
+    }
+}
---
+++ src/FlawBOT.Framework/Services/Search/AmiiboService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using FlawBOT.Framework.Common;
+using FlawBOT.Framework.Models;
+using FlawBOT.Framework.Properties;
+using Newtonsoft.Json;
+
+namespace FlawBOT.Framework.Services
+{
+    public class AmiiboService : HttpHandler
+    {
+        public static async Task<AmiiboData> GetAmiiboFigurineAsync(string query)
+        {
+            try
+            {
+                var results = await _http.GetStringAsync(Resources.API_Amiibo + "?name=" + query.ToLowerInvariant());
+                return JsonConvert.DeserializeObject<AmiiboData>(results);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
