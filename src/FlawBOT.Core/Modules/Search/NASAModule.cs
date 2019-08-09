6eedd794f609a8b16706428bdad8766f79299d09
CriticalFlaw <nik_101@live.co.uk>
2019-08-09 12:07:05 -0400

    Added NASA Command
    Added a command to retrieve the astrology pixture of the day using the NASA API

---
+++ src/FlawBOT.Core/Modules/Search/NASAModule.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using FlawBOT.Framework.Models;
+using FlawBOT.Framework.Services;
+
+namespace FlawBOT.Modules
+{
+    [Cooldown(3, 5, CooldownBucketType.Channel)]
+    public class NASAModule : BaseCommandModule
+    {
+        #region COMMAND_NASA
+
+        [Command("nasa")]
+        [Aliases("apod")]
+        [Description("Retrieve NASA's Astronomy Picture of the Day")]
+        public async Task NASA(CommandContext ctx)
+        {
+            var results = await NASAService.GetNASAImage();
+            if (results == null)
+                await BotServices.SendEmbedAsync(ctx, "Unable to reach NASA API", EmbedType.Missing);
+            else
+            {
+                var output = new DiscordEmbedBuilder()
+                    .WithTitle(results.Title)
+                    .WithDescription(results.Description)
+                    .WithImageUrl(results.ImageHD ?? results.ImageSD)
+                    .WithFooter(results.Copyright + " - " + results.Date)
+                    .WithColor(new DiscordColor("#0B3D91"));
+                await ctx.RespondAsync(embed: output.Build());
+            }
+        }
+
+        #endregion COMMAND_NASA
+    }
+}
---
+++ src/FlawBOT.Framework/Models/Search/NASAData.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace FlawBOT.Framework.Models
+{
+    public class NASAData
+    {
+        [JsonProperty("copyright")]
+        public string Copyright { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("explanation")]
+        public string Description { get; set; }
+
+        [JsonProperty("hdurl")]
+        public string ImageHD { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("url")]
+        public string ImageSD { get; set; }
+    }
+}
